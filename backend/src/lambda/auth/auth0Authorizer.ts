f6f5416c14ed587494f0ef6c473cae1a75eb3e97
synthetic.corpus <joel@joelgonzaga.com>
2020-11-11 09:50:00 -0800

    set authorization to return dummy data.

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,29 +1,35 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+// import { verify, decode } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+// import Axios from 'axios'
+// import { Jwt } from '../../auth/Jwt'
+// import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-jtg.us.auth0.com/.well-known/jwks.json'
+
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
+    /*
     const jwtToken = await verifyToken(event.authorizationToken)
     logger.info('User was authorized', jwtToken)
-
+    */
+    if(jwksUrl){
+      console.log("We have a web token URL, but it's not doing anything yet.")
+    }
     return {
-      principalId: jwtToken.sub,
+      // principalId: jwtToken.sub,
+      principalId: '123',
       policyDocument: {
         Version: '2012-10-17',
         Statement: [
@@ -54,6 +60,10 @@ export const handler = async (
   }
 }
 
+/*
+
+Will only implement this when I'm ready to do authentications
+
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
@@ -75,3 +85,5 @@ function getToken(authHeader: string): string {
 
   return token
 }
+
+*/
