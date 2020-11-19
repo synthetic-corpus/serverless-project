611a0c8435535129cf01af9e429f8dad4ab8bb75
synthetic.corpus <joel@joelgonzaga.com>
2020-11-18 17:34:42 -0800

    set configuration for client.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,12 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'dwfdynjdjk'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
+const AUTH0_CLIENT_ID = process.envAUTH0_CLIENT_ID
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: AUTH0_DOMAIN,            // Auth0 domain
+  clientId: AUTH0_CLIENT_ID,          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
