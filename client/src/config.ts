3946ce6efc13532c5f801a2fe18b9371fec722a4
synthetic.corpus <joel@joelgonzaga.com>
2020-11-18 17:51:30 -0800

    added auth links as strings instead of environment variables and it appears to work.

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,10 +3,11 @@ const apiId = 'dwfdynjdjk'
 export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
 
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
-const AUTH0_CLIENT_ID = process.envAUTH0_CLIENT_ID
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: AUTH0_DOMAIN,            // Auth0 domain
-  clientId: AUTH0_CLIENT_ID,          // Auth0 client id
+  domain: 'dev-jtg.us.auth0.com',            // Auth0 domain
+  clientId: 'r7v0Zdhl3oxgdo4KysS9cQIF4QkE8RMi',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
+console.log(authConfig)
