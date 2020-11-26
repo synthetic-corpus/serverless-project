1c2851fe3aeba2ed06ffb04ee2e1e98d037fa1bb
synthetic.corpus <joel@joelgonzaga.com>
2020-11-25 18:02:34 -0800

    modified for Cname endpoint.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'dwfdynjdjk'
-export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
+export const apiEndpoint = `https://cloudapi.joelgonzaga.com/dev`
 
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID
